dc53cebb7cf8b92436fe5059ce8bd5506abdeab0
Jay <jasoncarr@Jasons-MacBook-Pro.local>
2020-06-14 22:38:25 +0100

    Added gradient to helloworld app

--- src/hello-world/client/scripts/main.lua
+++ src/hello-world/client/scripts/main.lua
@@ -3,9 +3,22 @@
 print("Hello World!")
 print("I'm from a tevapp")
 
+teverse.construct("guiGradientFrame", {
+    backgroundColour = colour.rgb(84, 209, 117),
+    backgroundColourB = colour.rgb(25, 105, 46),
+    start = guiCoord(0, 0, 0, 0),
+    finish = guiCoord(1, 0, 1, 0),
+    parent = teverse.interface,
+    position = guiCoord(0.0, 0, 0.0, 0),
+    size = guiCoord(1.0, 0, 1.0, 0)
+})
+
 teverse.construct("guiTextBox", {
     parent = teverse.interface,
-    size = guiCoord(0, 100, 0, 50),
-    position = guiCoord(0.5, -50, 0.5, -25),
-    text = "Hello, I'm an app!"
+    size = guiCoord(0, 150, 0, 50),
+    position = guiCoord(0.5, -75, 0.5, -25),
+    text = "Hello, I'm an app!",
+    textAlign = "middle",
+    strokeRadius = 4,
+    zIndex = 2
 })
