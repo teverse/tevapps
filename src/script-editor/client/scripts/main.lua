f17d24913e625c13b8d478bff7f7c940582a9298
Jay <jasoncarr@Jasons-MacBook-Pro.local>
2020-06-15 21:11:02 +0100

    added run button to script editor

--- src/script-editor/client/scripts/main.lua
+++ src/script-editor/client/scripts/main.lua
@@ -1,3 +1,20 @@
 -- Main entry point for the app
-print("test")
-require("scripts/editor.lua").create()
+local container, editor = require("scripts/editor.lua").create()
+local theme = require("scripts/theme/default.lua")
+
+local run = teverse.construct("guiTextBox", {
+    parent = teverse.interface,
+    size = guiCoord(0, 100, 0, 50),
+    position = guiCoord(1, -100, 0, 0),
+    text = "RUN",
+    backgroundColour = theme.highlighted,
+    textColour = theme.foreground,
+    textSize = 32,
+    textFont = "tevurl:fonts/firaCodeBold.otf",
+    textAlign = "middle",
+    zIndex = 2
+})
+
+run:on("mouseLeftUp", function ()
+    pcall(loadstring(editor.text))
+end)
