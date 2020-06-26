218a679eff435f96a4f5a04c220d621f13b4e239
Jay <jasoncarr@Jasons-MacBook-Pro.local>
2020-06-26 11:26:42 +0100

    networked app

--- src/networked/client/scripts/main.lua
+++ src/networked/client/scripts/main.lua
@@ -1,1 +1,6 @@
 print("client main.lua")
+
+teverse.networking:on("customMessage", function(data1, data2)
+    print("MSG from server:", data1, data2)
+    teverse.networking:sendToServer("customMessage", "Hi from a client")
+end)
--- src/networked/server/scripts/main.lua
+++ src/networked/server/scripts/main.lua
@@ -1,1 +1,9 @@
-print("server main.lua")
+print("server main.lua!!")
+
+teverse.networking:on("customMessage", function(client, data1, data2)
+    print("MSG from client:", client, data1, data2)
+end)
+
+while sleep(1) do
+    teverse.networking:broadcast("customMessage", 10, 20)
+end
