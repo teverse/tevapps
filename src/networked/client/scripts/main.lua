0841580570cbcb5912e66d776ae4ce2801c28400
teverse <jay@teverse.com>
2020-07-01 21:18:03 +0100

    Updated network app

--- src/networked/client/scripts/main.lua
+++ src/networked/client/scripts/main.lua
@@ -5,7 +5,7 @@ uiController.createInterface()
 
 local function onChat(sender, message)
     local client = teverse.networking:getClient(sender)
-    uiController.addMessage(client and client.name or sender, client ~= nil and ("https://cdn.teverse.com/user/" .. client.id) or "test", message)
+    uiController.addMessage(client and client.name or sender, client ~= nil and ("https://cdn.teverse.com/user/" .. client.id) or "tevurl:img/info.png", message)
 end
 
 teverse.networking:on("chat", onChat)
@@ -19,36 +19,19 @@ teverse.networking:on("_clientDisconnected", function(client)
 end)
 
 teverse.networking:on("_disconnected", function(client)
-    uiController.addMessage("Server", "", "You have lost connection to the server, you won't be able to communicate!")
+    uiController.addMessage("Server", "tevurl:img/info.png", "You have lost connection to the server, you won't be able to communicate!")
 end)
 
 teverse.networking:on("_connected", function(client)
-    uiController.addMessage("Server", "", "You have connected to the server")
+    uiController.addMessage("Server", "tevurl:img/info.png", "You have connected to the server")
 end)
+
+-- Load the networking debug UI from the teverse/teverse github
+local debugContainer = require("tevgit:utilities/networkDebug.lua")()
+debugContainer.parent = teverse.interface
 
 -- Keep showing the loading UI until we're connected
 while not teverse.networking.isConnected do
     sleep(0.5)
 end
 uiController.loading:destroy()
-
-local dbg = teverse.construct("guiTextBox", {
-    parent = teverse.interface,
-    size = guiCoord(0, 200, 0, 250),
-    position = guiCoord(1, -210, 1, -260),
-    backgroundAlpha = 0,
-    textSize = 12,
-    textColour = colour(1, 0, 0),
-    textWrap = true,
-    textFont = "tevurl:fonts/firaCodeRegular.otf",
-    textAlign = "topLeft"
-})
-
-while sleep(0.7) do
-    local stats = teverse.networking:getStats()
-    dbg.text = "Packet loss (1s): " .. tonumber(stats.packetlossLastSecond) .. "\n"
-        .. "Packet loss (total):" .. tonumber(stats.packetlossTotal) .. "\n"
-        .. "Processed (total):" .. tonumber(stats.totalMessageBytesReceivedProcessed) .. "\n"
-        .. "Ignored (total):" .. tonumber(stats.totalMessageBytesReceivedIgnored) .. "\n"
-        .. "Send buffer:" .. tonumber(stats.bytesInSendBuffer) .. "\n"
-end
