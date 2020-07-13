b41246f5b56da95300f2d245bc1f4b81fbfc7bbb
teverse <jay@teverse.com>
2020-07-13 19:07:37 +0100

    Debug ui

--- src/networked-3d/client/scripts/main.lua
+++ src/networked-3d/client/scripts/main.lua
@@ -1,2 +1,6 @@
-teverse.scene.camera.position = vector3(0, 6, -15)
-teverse.scene.camera.rotation = quaternion.euler(math.rad(25), 0, 0)
+teverse.scene.camera.position = vector3(0, 6, -6)
+teverse.scene.camera.rotation = quaternion.euler(math.rad(35), 0, 0)
+
+-- Load the networking debug UI from the teverse/teverse github
+local debugContainer = require("tevgit:utilities/networkDebug.lua")()
+debugContainer.parent = teverse.interface
