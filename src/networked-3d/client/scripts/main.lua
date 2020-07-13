4e001c434691573f01c4a16e9fa79a60c28ac90f
teverse <jay@teverse.com>
2020-07-13 21:14:00 +0100

    networked-3d

--- src/networked-3d/client/scripts/main.lua
+++ src/networked-3d/client/scripts/main.lua
@@ -1,6 +1,24 @@
-teverse.scene.camera.position = vector3(0, 6, -6)
-teverse.scene.camera.rotation = quaternion.euler(math.rad(35), 0, 0)
+teverse.scene.camera.position = vector3(0, 2, -6)
+teverse.scene.camera.rotation = quaternion.euler(math.rad(0), 0, 0)
+teverse.scene.simulate = false
 
 -- Load the networking debug UI from the teverse/teverse github
 local debugContainer = require("tevgit:utilities/networkDebug.lua")()
 debugContainer.parent = teverse.interface
+
+-- Raycasting marker just for testing
+local marker = teverse.construct("block", {
+    position = vector3(0, 0, 0),
+    scale = vector3(0.2, 0.2, 0.2),
+    colour = colour(1, 1, 0),
+    simulated = false
+})
+
+local camera = teverse.scene.camera
+while sleep() do
+    local mPos = camera:screenToWorld(teverse.input.mousePosition)
+    local obj, pos, normal = teverse.scene:raycastOne(camera.position, camera.position + (mPos * 10000))
+    if obj then
+        marker.position = pos
+    end
+end
--- src/networked-3d/server/scripts/main.lua
+++ src/networked-3d/server/scripts/main.lua
@@ -3,12 +3,17 @@ teverse.scene.simulate = true
 
 local baseplate = teverse.construct("block", {
     position = vector3(0, 0, 0),
-    scale = vector3(30, 1, 30),
+    scale = vector3(6, 0.1, 6),
     colour = colour(1, 1, 1)
 })
 
 local block = teverse.construct("block", {
     position = vector3(0, 5, 0),
     scale = vector3(1, 1, 1),
-    colour = colour(1, 0, 1)
+    colour = colour(1, 0, 1),
+    static = false
 })
+
+while sleep(4) do
+    block.position = vector3(0, 5, 0)
+end
