b77fa549338b5187f979ac1989b48d40a42f7f12
Jay <jasoncarr@Jasons-MacBook-Pro.local>
2020-08-16 11:04:11 +0100

    glb update

--- src/glb/client/scripts/main.lua
+++ src/glb/client/scripts/main.lua
@@ -1,13 +1,19 @@
-teverse.scene.camera.position = vector3(0, -130, 100)
-teverse.scene.camera:lookAt(vector3(0, -130, 0))
+teverse.scene.camera.position = vector3(2, 1, 1)
+teverse.scene.camera:lookAt(vector3(0, 0.6, 0))
 
 local rot = teverse.scene.camera.rotation
 local light = teverse.construct("directionalLight", {
-    rotation = quaternion.euler(math.rad(120), 0, 0),
+    rotation = quaternion.euler(math.rad(-140), math.rad(-45), 0),
     colour = colour(5, 5, 5)
 })
 
-local block = teverse.construct("block", {
+teverse.construct("block", {
+    position = vector3(0, -1, 0),
+    scale = vector3(100,0.2,100),
+    colour = colour(0, 0, 1)
+})
+
+local man = teverse.construct("block", {
     position = vector3(0, 0, 0),
     scale = vector3(1,1,1),
     colour = colour(1, 1, 1),
@@ -16,4 +22,22 @@ local block = teverse.construct("block", {
 })
 
 local camera = require("tevgit:core/3d/camera.lua") -- 3D Camera for 3D Environment
-block.scale = block.meshScale
+man.scale = man.meshScale
+
+
+local legJoint3 = man:getJoint("mixamorig:Spine2")
+local start3 = legJoint3.rotation
+
+while sleep(1) do
+    teverse.tween:begin(legJoint3, 0.5, {
+        rotation = legJoint3.rotation * quaternion.euler(math.rad(20), math.rad(24), math.rad(-82))
+    }, "inOutQuad")
+
+
+    sleep(1)
+
+    teverse.tween:begin(legJoint3, 0.5, {
+        rotation = start3
+    }, "inOutQuad")
+
+end
